2c45799d8a158b374e5963bffc3edb2310c6dfd4
swarupajadhav <swarupaj120@gmail.com>
2025-05-31 11:09:18 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 24 09:48:05 2025
+
+@author: Admin
+"""
+
+age=int(input("Enter age"))
+if age<=12:
+    print("Child")
+elif age<=19:
+    print("Teenager")
+elif age<35:
+    print("Younger")
+else:
+    print("Adult")
