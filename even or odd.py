aa338289a46c57fdefe9a2d7f983b5ce6f8eefbc
swarupajadhav <swarupaj120@gmail.com>
2025-05-31 10:58:27 +0530

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 24 09:45:09 2025
+
+@author: Admin
+"""
+
+num=int(input("Enter number"))
+if num%2==0:
+    print("Number is even")
+else:
+    print("Number is odd")
